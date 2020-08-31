eac3525588e912cdd29d38a2b7a884689491ff3a
Robin Ricard <git@rricard.me>
2020-08-31 11:16:12 +0100

    Setup IDE

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,6 @@
 #[cfg(feature = "js_v8")]
-mod js_v8;
-
-#[cfg(feature = "js_v8")]
 #[path = "js_v8/mod.rs"]
 mod backend;
 
-#[cfg(feature = "js_wasm")]
-compile_error!("`js_wasm` backend not implemented yet");
-
 #[cfg(not(any(feature = "js_v8", feature = "js_wasm")))]
-compile_error!("Please select at least a feature to build a default backend: `js_v8`, `js_wasm`");
+compile_error!("Please select one feature to build a default backend: `js_v8`, `js_wasm`");
