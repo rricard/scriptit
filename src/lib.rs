69fa039a064579ace422fd17f975c5b332dc6051
Robin Ricard <git@rricard.me>
2020-08-31 14:10:22 +0100

    Redo feature switch to be platform dependent

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,11 @@
 pub mod core;
 
-#[cfg(feature = "js_v8")]
+#[cfg(not(target_arch = "wasm32"))]
 #[path = "js_v8/mod.rs"]
 mod backend;
 
-#[cfg(not(any(feature = "js_v8", feature = "js_wasm")))]
-compile_error!("Please select one feature to build a default backend: `js_v8`, `js_wasm`");
+#[cfg(target_arch = "wasm32")]
+#[path = "js_wasm/mod.rs"]
+mod backend;
 
 pub use backend::ScriptingEnvironment;
