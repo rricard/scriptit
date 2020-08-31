0e2998b57b0abd0fc03d366317f01b697db17b42
Robin Ricard <git@rricard.me>
2020-08-31 11:07:39 +0100

    Feature loading to select main backend

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,12 @@
 #[cfg(feature = "js_v8")]
 mod js_v8;
+
+#[cfg(feature = "js_v8")]
+#[path = "js_v8/mod.rs"]
+mod backend;
+
+#[cfg(feature = "js_wasm")]
+compile_error!("`js_wasm` backend not implemented yet");
+
+#[cfg(not(any(feature = "js_v8", feature = "js_wasm")))]
+compile_error!("Please select at least a feature to build a default backend: `js_v8`, `js_wasm`");
