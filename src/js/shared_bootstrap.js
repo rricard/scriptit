39a8527f395ff8fc05ae901089c99d30c20d254b
Robin Ricard <git@rricard.me>
2020-09-27 11:47:12 +0100

    Simplify interface for now

--- src/js/shared_bootstrap.js
+++ src/js/shared_bootstrap.js
@@ -3,8 +3,8 @@
 /**
  * Calls synchronously a rust primtive handler
  * @param {string} handler Name of the primitive handler
- * @param {Uint8Array} data Bytes to send to the primitive handler
- * @returns {Uint8Array} Bytes received from the primitive handler
+ * @param {string} data String Data to send to the primitive handler
+ * @returns {string} String Data received from the primitive handler
  */
 function callToRust(handler, data) {
     // Placeholder function that will be replaced by Rust
